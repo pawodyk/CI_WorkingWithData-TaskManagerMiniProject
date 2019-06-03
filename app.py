640bf82f00d2304a005c2cc3e00f78368b9aaa09
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-03 16:40:46 +0000

    setup basic structure of the page templates

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DB')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/get_task')
+def get_task():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
     
 
 if __name__ == "__main__":
