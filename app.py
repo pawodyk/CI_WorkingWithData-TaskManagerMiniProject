c4cb862ea8f3e0943371a73aa23ef22294e603f1
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-05 18:50:02 +0000

    Added functionality - editing the task; fixed error in Procfile

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DB')
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_DBNAME"] = os.getenv('MONGO_DB')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
@@ -22,17 +22,37 @@ def add_task():
     
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks= mongo.db.tasks
-    tasks.insert_one(request.form.to_dict())
+    tasks = mongo.db.tasks
+    form_data = request.form.to_dict()
+    tasks.insert_one(form_data)
     print("***")
     print("sucessfully added:")
-    print(request.form.to_dict())
+    print(form_data)
     print("***")
     return redirect(url_for('get_task'))
     
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    
+    return render_template('edittask.html', task=the_task, categories=all_categories)
+    
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_task'))
 
 if __name__ == "__main__":
-    app.run(host = os.environ.get('IP', '127.0.0.1'), 
-            port = int(os.environ.get('PORT', '5500')),
+    app.run(host = os.getenv('IP', '127.0.0.1'), 
+            port = int(os.getenv('PORT', '5500')),
             debug=True)
             
