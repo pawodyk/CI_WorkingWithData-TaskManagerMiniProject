f04f71d048e1f1028e056040152e862cb1061833
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-05 20:34:29 +0000

    added funcionality to handle task categories

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+## Handling Tasks ## 
 
 @app.route('/')
 @app.route('/get_task')
@@ -35,7 +36,6 @@ def insert_task():
 def edit_task(task_id):
     the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories =  mongo.db.categories.find()
-    
     return render_template('edittask.html', task=the_task, categories=all_categories)
     
 @app.route('/update_task/<task_id>', methods=["POST"])
@@ -51,12 +51,49 @@ def update_task(task_id):
     })
     return redirect(url_for('get_task'))
     
-    
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_task'))
 
+
+## Handling Categories ##
+@app.route('/get_categories')
+def get_categories():
+    _categories = mongo.db.categories.find()
+    categories_list = [category for category in _categories]
+    return render_template("categories.html", categories = categories_list)
+    
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one(
+                           {'_id': ObjectId(category_id)}))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+    
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
+
+
+## Handling Code execution ## 
 if __name__ == "__main__":
     app.run(host = os.getenv('IP', '127.0.0.1'), 
             port = int(os.getenv('PORT', '5500')),
