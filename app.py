0d3cae7ef2ef2a3ee99c01fbaa38ac3266b8ed89
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-05 11:37:56 +0000

    added the add task page with the fields coresponding to the mongodb fields

--- app.py
+++ app.py
@@ -16,8 +16,14 @@ mongo = PyMongo(app)
 def get_task():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
     
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+    
 
 if __name__ == "__main__":
     app.run(host = os.environ.get('IP', '127.0.0.1'), 
             port = int(os.environ.get('PORT', '5500')),
             debug=True)
+            
+    print(MONGO_DB)
